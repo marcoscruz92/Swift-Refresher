c29b96185fe3ae7cff658c683fdd51fd3d11d56d
Marcos <marcoscruz1992@gmail.com>
2015-11-24 23:00:59 -0400

    first commit

---
+++ Exercise - Variable Operations.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var val1 = 15
+var val2 = 5
+var val3 = val1 + val2
+var val4 = 5 * 9
+var val5 = val1 - val2
+var val6 = val1 / val2
+var val7 = 7, val8 = 8, val9 = 9
+let val10 = 10, val11 = 11, val12 = 12
+var valDouble: Double
+var valInt: Int = 423
+var hello: String = "Hello"
+var world: String = "World"
+var helloWorld1 = hello + " " + world
+var helloWorld2 = "\(hello) \(world)"
